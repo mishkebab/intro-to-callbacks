a10cfc683bfcc51f36140c8eb46f45c0c38c764e
misha <mishabansal7@gmail.com>
2023-05-17 15:12:29 -0700

    finished mybind

--- intro_to_callbacks.js
+++ intro_to_callbacks.js
@@ -65,4 +65,40 @@ function addNumber(sum, numsLeft, callback){
     };
 };
 
-addNumber(0, 3, sum => console.log(`Total Sum: ${sum}`));
+// addNumber(0, 3, sum => console.log(`Total Sum: ${sum}`));
+
+
+Function.prototype.myBind = function(context) {
+    const func = this;
+
+    function binding() {
+        // console.log(this);
+        func.apply(context);
+    }
+
+    // const binding = () => {
+    //     this.apply(context);
+    // };
+    return binding;
+};
+
+
+class Lamp {
+    constructor() {
+      this.name = "a lamp";
+    }
+  }
+  
+const turnOn = function() {
+console.log("Turning on " + this.name);
+};
+  
+  const lamp = new Lamp(); // this == Lamp.new(self.name)
+  
+//   turnOn(); // should not work the way we want it to
+  
+//   const boundTurnOn = turnOn.bind(lamp);
+  const myBoundTurnOn = turnOn.myBind(lamp);
+  
+//   boundTurnOn(); // should say "Turning on a lamp"
+  myBoundTurnOn(); // should say "Turning on a lamp"
